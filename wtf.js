4c485e8e46439261b7da7e9a7c5f63bf0baab1e9
andraluca <andralucaseattle@gmail.com>
2020-06-24 16:02:30 -0700

    who knows what I am doing raise their hand

---
+++ wtf.js
@@ -0,0 +1,9 @@
+var dogs = prompt('Have you owned a dog before?');
+var feedback;
+    if (dogs = yes) {
+        feedback = 'You do not need convincing!';
+    } else if (dogs = no) {
+        feedback = 'We will assume you are already a sweet person.';
+    } else {feedback = 'Please enter Yes or No'
+    }
+    
