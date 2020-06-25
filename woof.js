7be91c242e824d527c1e700606becb2a02d61d71
andraluca <andralucaseattle@gmail.com>
2020-06-25 12:55:33 -0700

    cleaning up some words

--- woof.js
+++ woof.js
@@ -2,24 +2,24 @@
 
 
 function askUserName(){
-    var internalUserName = prompt('What is your name?');
-    document.write('<h2>' + 'Welcome '+ internalUserName + '</h2>');
-    '<h2>Welcome Roger</h2>'
+    var internalUserName = prompt('what is your name?');
+    document.write('<h2>' + 'welcome '+ internalUserName + '</h2>');
+    '<h2>welcome Andra</h2>'
     return internalUserName;
 }
 
 function confirmContinue(userName){
-    confirm(userName + "let's check to see if my web page is for you");
+    confirm(userName + '-' + "let's check to see if my web page is for you");
 }
 
-function lightSide(){
-    var lightSide;
-    lightSide = prompt('Are you comeone who is uncomfortable with hard work?');
-    return lightSide
+function hardWork(){
+    var hardWork;
+    hardWork = prompt('Are you someone who despises hard work?');
+    return hardWork
 }
 
-function checkLight(lightSideQuestion){
-    if(lightSideQuestion === 'yes'){
+function checkAnswer(HardWorkQuestion){
+    if(HardWorkQuestion === 'yes'){
         alert('I am sorry to hear.  Please never ever get a puppy!')
     } else {
         alert('Happy to know you understand the commitment involved!');
@@ -43,5 +43,5 @@ function adopt(lightSideQuestion){
 var externalUserName = askUserName();
 confirmContinue(externalUserName);
 var lightSideQuestion = lightSide();
-checkLight(lightSideQuestion);
-adopt(lightSideQuestion);
+checkAnswer(HardWorkQuestion);
+adopt(HardWorkQuestion);
