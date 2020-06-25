f967217eee8b584af58f18d435a8ef9717a07c54
andraluca <andralucaseattle@gmail.com>
2020-06-24 19:04:48 -0700

    praying to all angels that this code works

---
+++ woof.js
@@ -0,0 +1,8 @@
+var human = prompt('This is a test, are you a human?');
+var welcome;
+    if (human = "yes") {
+        welcome = "Welcome to our site! Please check out our doggos!";
+    } else if (human != null) {
+        welcome = "We apologize, robots cannot own dogs.";
+    }
+    document.write(welcome);
