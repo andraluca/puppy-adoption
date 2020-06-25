8bfdf274af7be365bfd14bc89058d45939561af2
andraluca <andralucaseattle@gmail.com>
2020-06-24 17:35:51 -0700

    victorious is getting the darn thig to work

--- wtf.js
+++ wtf.js
@@ -1,8 +1,9 @@
-var dogs = prompt('Have you owned a dog before?');
+var dogs = prompt('How many pets or dogs have you owned before?');
 var feedback;
-    if (dogs = "yes") {
-        feedback = 'You do not need convincing!';
-    } else if (dogs = "no") {
+    if (dogs > 1) {
+        feedback = 'You will make a great puppy parent!';
+    } else if (dogs <= 1) {
         feedback = 'We will assume you are already a sweet person.';
-    }
+    } else {feedback = 'Please enter a valid number';
+}
     document.write(feedback);
