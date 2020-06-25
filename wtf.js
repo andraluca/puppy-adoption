46bb8feaa6a09258272518703895eed32e7540ac
andraluca <andralucaseattle@gmail.com>
2020-06-24 17:14:56 -0700

    added quotations to yes and no answers

--- wtf.js
+++ wtf.js
@@ -1,8 +1,8 @@
 var dogs = prompt('Have you owned a dog before?');
 var feedback;
-    if (dogs = yes) {
+    if (dogs = "yes") {
         feedback = 'You do not need convincing!';
-    } else if (dogs = no) {
+    } else if (dogs = "no") {
         feedback = 'We will assume you are already a sweet person.';
     }
     document.write(feedback);
