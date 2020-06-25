b057553664c38fb52174a042aab86e20307c82bb
andraluca <andralucaseattle@gmail.com>
2020-06-24 19:28:30 -0700

    playing with the statements

--- woof.js
+++ woof.js
@@ -1,8 +1,9 @@
-var human = prompt('This is a test, are you a human, type Woof?');
+var human = prompt('This is a test, type woof to prove you are a human!');
 var welcome;
     if (human = "woof") {
         welcome = "Welcome to our site! Please check out our doggos!";
     } else if (human != null) {
-        welcome = "We apologize, are not warm and cuddly.";
+        welcome = "We apologize, robots do not make great parents.";
     }
     document.write(welcome);
+    //I am still trying to sort out why my second message is not displaying//
